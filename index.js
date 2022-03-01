917a21a73bf2cbadf1725dfa8082b9e6274d9e8d
mrjeoffrey <b.jeoffrey@gmail.com>
2022-02-28 20:48:32 -0800

    added prompts

--- index.js
+++ index.js
@@ -5,20 +5,100 @@ const path = require("path");
 
 // inquirer prompts #################################
 
+const prompts = {
+	// WHEN I start the application
+	// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
+	manager: [
+		{
+			type: `input`,
+			name: `managerName`,
+			message: `What is your manager's name?`,
+		},
+		{
+			type: `input`,
+			name: `managerId`,
+			message: `What is your manager's ID?`,
+		},
+		{
+			type: `input`,
+			name: `managerEmail`,
+			message: `What is your manager's email?`,
+		},
+		{
+			type: `input`,
+			name: `managerOfficeNumber`,
+			message: `What is your manager's office number?`,
+		},
+	],
+
+	// WHEN I select the engineer option
+	// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+
+	engineer: [
+		{
+			type: `input`,
+			name: `engineerName`,
+			message: `What is your engineer's name?`,
+		},
+		{
+			type: `input`,
+			name: `engineerId`,
+			message: `What is your engineer's ID?`,
+		},
+		{
+			type: `input`,
+			name: `engineerEmail`,
+			message: `What is your engineer's email?`,
+		},
+		{
+			type: `input`,
+			name: `engineerGithub`,
+			type: `What is your engineer's GitHub?`,
+		},
+	],
+
+	// WHEN I select the intern option
+	// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+
+	intern: [
+		{
+			type: `input`,
+			name: `internName`,
+			message: `What is your intern's name?`,
+		},
+		{
+			type: `input`,
+			name: `internId`,
+			message: `What is your intern's ID?`,
+		},
+		{
+			type: `input`,
+			name: `internEmail`,
+			message: `What is your intern's email?`,
+		},
+		{
+			type: `input`,
+			name: `internSchool`,
+			message: "What school does your intern attend?",
+		},
+	],
+
+	// WHEN I enter the team manager’s name, employee ID, email address, and office number
+	// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+
+	newMember: [
+		{
+			type: `list`,
+			name: `newMember`,
+			message: `What would you like to do?`,
+			choices: [`Add Engineer`, `Add Intern`, `Finish building team`],
+		},
+	],
+};
+
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for my team members and their information
-
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 
 // build index.html file #################################
 
