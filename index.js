8259a4f622c8aef4d4ef459b7d4ca0e53bf6e237
mrjeoffrey <b.jeoffrey@gmail.com>
2022-02-28 23:49:21 -0800

    modified constructors

--- index.js
+++ index.js
@@ -3,6 +3,71 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 
+// constructors
+
+class Employee {
+	constructor(name, id, email) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+	}
+	getName() {
+		return this.name;
+	}
+
+	getId() {
+		return this.id;
+	}
+	getEmail() {
+		return this.email;
+	}
+	getRole() {
+		return "Employee";
+	}
+}
+
+class Engineer extends Employee {
+	constructor(name, id, email, github) {
+		super(name, id, email);
+		this.github = github;
+	}
+
+	getRole() {
+		return "Engineer";
+	}
+	getGithub() {
+		return this.github;
+	}
+}
+
+class Intern extends Employee {
+	constructor(name, id, email, school) {
+		super(name, id, email);
+		this.school = school;
+	}
+
+	getSchool() {
+		return this.school;
+	}
+	getRole() {
+		return "Intern";
+	}
+}
+
+class Manager extends Employee {
+	constructor(name, id, email, officeNumber) {
+		super(name, id, email);
+		this.officeNumber = officeNumber;
+	}
+	getOfficeNumber() {
+		return this.officeNumber;
+	}
+
+	getRole() {
+		return "Manager";
+	}
+}
+
 // inquirer prompts #################################
 
 const prompts = {
