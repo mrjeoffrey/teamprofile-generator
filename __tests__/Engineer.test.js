54753e00ac444bb72e2b3a19a4835ca83baa22d3
mrjeoffrey <b.jeoffrey@gmail.com>
2022-03-02 17:18:52 -0800

    refactored index-template.html, added engineer test. passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,15 +1,42 @@
+"use strict";
 // The first class is an Employee parent class with the following properties and methods:
+
 // name
 // id
 // email
 // getName()
 // getId()
 // getEmail()
-// getRole()—returns 'Employee'
-// In addition to Employee's properties and methods, Engineer will also have the following:
 
-// github—GitHub username
+// all Employee requirements met inside Engineer.js. Engineer.js imports Employee.js
+const Engineer = require("../lib/Engineer");
 
-// getGithub()
+describe("Engineer Constructor", () => {
+	describe("Get() Functions Tests", () => {
+		// getRole()—returns 'Employee'
+		// In addition to Employee's properties and methods, Engineer will also have the following:
+		// github—GitHub username
 
-// getRole()—overridden to return 'Engineer'
+		it("can set GitHUb account via constructor", () => {
+			const testValue = "GitHubUser";
+			const engineer = new Engineer("Foo", 1, "test@test.com", testValue);
+			expect(engineer.github).toBe(testValue);
+		});
+
+		// getGithub()
+
+		it("can get GitHub username via getGithub()", () => {
+			const testValue = "GitHubUser";
+			const engineer = new Engineer("Foo", 1, "test@test.com", testValue);
+			expect(engineer.getGithub()).toBe(testValue);
+		});
+
+		// getRole()—overridden to return 'Engineer'
+
+		it('getRole() should return "Engineer"', () => {
+			const testValue = "Engineer";
+			const engineer = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+			expect(engineer.getRole()).toBe(testValue);
+		});
+	});
+});
